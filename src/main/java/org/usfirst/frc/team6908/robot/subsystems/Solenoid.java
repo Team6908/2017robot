b071c2a629dc33cc25d2d9aa0fea76e53788de11
rohinisharma <rasberry314@gmail.com>
2017-12-31 13:47:28 -0500

    fixed minor compilation errors

--- src/main/java/org/usfirst/frc/team6908/robot/subsystems/Solenoid.java
+++ src/main/java/org/usfirst/frc/team6908/robot/subsystems/Solenoid.java
@@ -3,8 +3,9 @@ package org.usfirst.frc.team6908.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Solenoid extends Subsystem
-{
+public class Solenoid extends Subsystem {
+	
+	
 	public static void toggle(DoubleSolenoid piston)
 	{
 		if(piston.get() == DoubleSolenoid.Value.kForward)
@@ -24,3 +25,4 @@ public class Solenoid extends Subsystem
 	}
 	
 }	
+	
